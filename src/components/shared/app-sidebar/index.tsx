2d93af7508a00193ac127178dade1da3988c5251
unknown <higorrodrigues44@outlook.com>
2025-08-15 16:49:16 -0300

    Adicionado o nav-itms e o nav-user

--- src/components/shared/app-sidebar/index.tsx
+++ src/components/shared/app-sidebar/index.tsx
@@ -2,6 +2,11 @@ import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } fr
 import Link from "next/link";
 import { ComponentProps } from "react";
 
+import Logo from "@/assets/logo.svg"
+import LogoIcon from "@/assets/logo-icon.svg"
+import { NavItems } from "./nav-items";
+import { NavUser } from "./nav-user";
+
 type AppSidebarProps = ComponentProps<typeof Sidebar>
 
 export const AppSidebar = ({ ...props }: AppSidebarProps) => {
@@ -9,11 +14,16 @@ export const AppSidebar = ({ ...props }: AppSidebarProps) => {
         <Sidebar collapsible="icon" {...props}>
             <SidebarHeader className="py-4">
                 <Link href="/">
-                    <p>Logo</p>
+                    <Logo className="w-full max-w-[150px] mx-auto pt-3 sm:hidden group-data-[state=expanded]:block"/>
+                    <LogoIcon className="w-full max-w-[20px] mx-auto pt-3 hidden group-data-[state=collapsed]:block"/>
                 </Link>
             </SidebarHeader>
-            <SidebarContent></SidebarContent>
-            <SidebarFooter></SidebarFooter>
+            <SidebarContent>
+                <NavItems></NavItems>
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser></NavUser>
+            </SidebarFooter>
             <SidebarRail/>
         </Sidebar>
     )
